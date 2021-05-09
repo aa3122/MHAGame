3e42c8256496abcb65472e78c384d71e2703b5c3
Will <wphogan603@gmail.com>
2021-05-09 17:23:17 -0400

    testing html request on student page

--- student/views.py
+++ student/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def studentDashboard(request):
-    return HttpResponse("You're at the student dashboard")
-    #return render(request, 'studentDashboard.html')
+    #return HttpResponse("You're at the student dashboard")
+    return render(request, 'studentDashboard.html')
 
 def calender(request):
-    return HttpResponse("You're on the Student view of the Calender page")
-    #return render(request, 'studentCalender.html')
+    #return HttpResponse("You're on the Student view of the Calender page")
+    return render(request, 'studentCalender.html')
 
 def gameInput(request):
-    return HttpResponse("You're at the student view of the Game Input")
-    #return render(request, 'studentGameInput.html')
+    #return HttpResponse("You're at the student view of the Game Input")
+    return render(request, 'studentGameInput.html')
 
 def studentGameOverview(request):
-    return HttpResponse("You're at the student view of the Game Overview")
-    #return render(request, 'studentGameOverview.html')
+    #return HttpResponse("You're at the student view of the Game Overview")
+    return render(request, 'studentGameOverview.html')
 
 
 
