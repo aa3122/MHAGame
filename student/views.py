052bbe93e96f2b92eb33a22819c0c2533fafd61c
Will <wphogan603@gmail.com>
2021-05-09 17:13:26 -0400

    added views

--- student/views.py
+++ student/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def studentDashboard(request):
+    return HttpResponse("You're at the student dashboard")
+    #return render(request, 'studentDashboard.html')
+
+def calender(request):
+    return HttpResponse("You're on the Student view of the Calender page")
+    #return render(request, 'studentCalender.html')
+
+def gameInput(request):
+    return HttpResponse("You're at the student view of the Game Input")
+    #return render(request, 'studentGameInput.html')
+
+def studentGameOverview(request):
+    return HttpResponse("You're at the student view of the Game Overview")
+    #return render(request, 'studentGameOverview.html')
+
+
+
