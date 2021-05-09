d78958bb84c2a4e260fca5f03da4b427e5263557
Andrew Ayers <aa3122@uncw.edu>
2021-05-09 16:38:28 -0400

    app addition

--- MHAGame/settings.py
+++ MHAGame/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MHAGame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [/templates],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
