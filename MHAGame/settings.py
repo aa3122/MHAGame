9a22695f24dde940d8853fad949fd86d7484cc40
Will <wphogan603@gmail.com>
2021-05-09 17:31:50 -0400

    Added: admin,student, game and login to installed apps fixed template directory

--- MHAGame/settings.py
+++ MHAGame/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin',
+    'student',
+    'game',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'MHAGame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
