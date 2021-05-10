2065e9e329a9aede9f52f9a8db703952c1a07b3b
Will <wphogan603@gmail.com>
2021-05-10 13:48:49 -0400

    fixed admin urls

--- MHAGame/urls.py
+++ MHAGame/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path('administrator/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('student/', include('student.urls')),
     path('login/', include('login.urls'))
     #path('game/', include('game.urls'))
