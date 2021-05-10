33131ae6a9deb1a9e9fecbdfa1ae500c440f45e3
Andrew Ayers <aa3122@uncw.edu>
2021-05-09 23:40:01 -0400

    BootsStrap stuff

--- MHAGame/settings.py
+++ MHAGame/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin',
     'student',
     'game',
     'login',
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'MHAGame.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
