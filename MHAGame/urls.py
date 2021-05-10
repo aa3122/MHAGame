a349ac8b7e255919c99514e936cb77fe2755b658
Will <wphogan603@gmail.com>
2021-05-10 12:55:22 -0400

    fixed urlpatterns syntax error

--- MHAGame/urls.py
+++ MHAGame/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('administrator/', admin.site.urls),
+    #path('administrator/', admin.site.urls),
     path('student/', include('student.urls')),
-    path('login/', include('login.urls')),
-    path('game/', include('game.urls'))
+    path('login/', include('login.urls'))
+    #path('game/', include('game.urls'))
 ]
