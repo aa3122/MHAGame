496c5ea456af57569760e162a93c107d6e62f91a
Will <wphogan603@gmail.com>
2021-05-10 15:19:37 -0400

    launching login.html

--- login/views.py
+++ login/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return HttpResponse("you're at the login page :)")
-    #return render(request, 'studentDashboard.html')
+    #return HttpResponse("you're at the login page :)")
+    return render(request, 'login.html')
 
 def forgotpassword(request):
-    return HttpResponse("you forgot your password :(")
-    #return render(request, 'studentDashboard.html')
+    #return HttpResponse("you forgot your password :(")
+    return render(request, 'forgotpassword.html')
 
 
