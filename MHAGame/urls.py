c60597ea8bd0b0272ce96a2168ee6f6f34f16688
Will <wphogan603@gmail.com>
2021-05-10 12:21:30 -0400

    debugging urls/view for login

--- MHAGame/urls.py
+++ MHAGame/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('student/', include('student.urls'))
+    path('administrator/', admin.site.urls),
+    path('student/', include('student.urls'),
+    path('login/', include('student.urls'),
+    path('game/', include('student.urls'))
 ]
