e9c8674df0df9b5d664dacf42326638fe23eb9fa
Will <wphogan603@gmail.com>
2021-05-10 12:53:36 -0400

    fixed urlpatterns syntax error

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="loginpage"),
+    path('', views.login, name="login"),
     path('forgotpassword', views.forgotpassword, name="forgotpassword")
 
 ]
