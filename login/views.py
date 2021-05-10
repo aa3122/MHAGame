bf73483dbcdddb49bdb0f522180592339b7417ea
Alejandra O <alejandranomalley@gmail.com>
2021-05-10 15:23:55 -0400

    hello

--- login/views.py
+++ login/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import *
+from django.shortcuts import redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 
 def login(request):
     return HttpResponse("you're at the login page :)")
@@ -9,4 +15,10 @@ def forgotpassword(request):
     return HttpResponse("you forgot your password :(")
     #return render(request, 'studentDashboard.html')
 
+def loginpage(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
 
