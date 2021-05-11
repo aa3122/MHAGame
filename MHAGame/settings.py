1f8ecf07e0d369bf9ab16749954a60dd70f5bbfc
nickp <np6633@uncw.edu>
2021-05-11 13:48:32 -0400

    added an A

--- MHAGame/settings.py
+++ MHAGame/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/MHGame/static/'
+STATIC_URL = '/MHAGame/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIR = (
