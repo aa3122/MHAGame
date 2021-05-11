7e275769fea327a50dfb49fef55e5083b13be006
Alejandra O <alejandranomalley@gmail.com>
2021-05-11 13:45:15 -0400

    commit message

--- administration/urls.py
+++ administration/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-#urlpatterns = [
-#    path('', views.administration, name='administration')
+urlpatterns = [
+    path('', views.adminDashboard, name='administration')
 
-#]
+]
