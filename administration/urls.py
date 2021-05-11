d49d6fd53f6edf9846968b66eb1fee41cd5dc72c
Alejandra O <alejandranomalley@gmail.com>
2021-05-11 16:09:29 -0400

    reset

--- administration/urls.py
+++ administration/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.adminDashboard, name='administration')
+    path('', views.adminDashboard, name='administration'),
+    path('creategame', views.adminCreateGame, name='adminCreateGame'),
+    path('viewgames', views.adminViewGame, name='adminViewGame'),
+    path('viewstudents', views.adminViewStudents, name='adminViewStudents')
 
 ]
--- administration/views.py
+++ administration/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def adminDashboard(request):
-    return HttpResponse("you're at the administration dashboard :)")
-    #return render(request, 'login.html')
+	#return HttpResponse("you're at the administration dashboard :)")
+	return render(request, 'adminDashboard.html')
+
+def adminCreateGame(request):
+	return render(request, 'adminCreateGame.html')
+
+def adminViewGame(request):
+	return render(request, 'adminViewGame.html')
+
+def adminViewStudents(request):
+	return render(request, 'adminViewStudents.html')
